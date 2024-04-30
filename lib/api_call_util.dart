a3e71e68b0dde2a09a3ba1f9140d392fa464b773
sashish <sashish@rediff-inc.com>
2024-05-01 01:23:16 +0530

    commented

--- lib/api_call_util.dart
+++ lib/api_call_util.dart
@@ -7,8 +7,8 @@ class ApiUtils {
    // 'Access-Control-Allow-Origin': '*',
    // 'Access-Control-Allow-Methods': 'POST',
    // 'Access-Control-Allow-Headers': 'X-Requested-With',
-    'Content-Type': 'application/json',
-    'Accept':'application/json',
+  //  'Content-Type': 'application/json',
+  //  'Accept':'application/json',
     'access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzE2NzU3NTI5LCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMDMyMzU2OSJ9.vFZDPb5I7BOHnKfg88lw8DvRUuXxO0Lc2JAbs7cuyXX5BKA-9fgBns8Zo2iwdO2jfi0f8-4ocNdlfX9O-Kq7-Q'
   };
   Future<void> makePostApiCall(String url, {Map<String, String>? data}) async {
