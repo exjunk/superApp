dc20809325c0e3171b8ee346557d37c1ff1431fc
sashish <sashish@rediff-inc.com>
2024-07-10 16:17:41 +0530

    api change for AWS

--- lib/my_const.dart
+++ lib/my_const.dart
@@ -5,5 +5,5 @@
   const String sensex = "SENSEX";
   const String optionTypeCE = "CE";
   const String optionTypePE = "PE";
-  const String apiBaseUrl = "http://127.0.0.1:5000/";
+  const String apiBaseUrl = "https://15.207.51.28";//"http://127.0.0.1:5000/";
   const String dhanClientId = "1100323569";
