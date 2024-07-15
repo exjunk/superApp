892173615d948db9a23815f53d979e36cb0674e5
sashish <sashish@rediff-inc.com>
2024-07-15 13:06:14 +0530

    latest build released for aws2

--- lib/my_const.dart
+++ lib/my_const.dart
@@ -5,6 +5,6 @@
   const String sensex = "SENSEX";
   const String optionTypeCE = "CE";
   const String optionTypePE = "PE";
-  const String apiBaseUrl = "http://3.109.144.42:8000";//"http://127.0.0.1:5000/";
-  const String socketBaseUrl = "http://3.109.144.42:5000"; //http://localhost:5000
+  const String apiBaseUrl = "https://3.109.144.42:8000";//"http://127.0.0.1:5000/";
+  const String socketBaseUrl = "https://3.109.144.42:5000"; //http://localhost:5000
   const String dhanClientId = "1100323569";
